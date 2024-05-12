a4aaac218d24203b54662c9ea2cdfd8f01dd6ebd
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-05-12 22:38:34 +0200

    Mapping forecast data in React component

--- frontend/src/components/WeatherData.tsx
+++ frontend/src/components/WeatherData.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import MyGridComponent from "./GridComponent";
 
 const WeatherData = () => {
   const [forecast, setForecast] = useState(null);
@@ -19,10 +20,40 @@ const WeatherData = () => {
 
   return (
     <>
-      <form>
-        {forecast &&
-          forecast.map((date) => <li key={date.date}>{date.date}</li>)}
-      </form>
+      <table>
+        <tbody>
+          {forecast &&
+            forecast.map((date, index) => {
+              // Parse the date string
+              const parsedDate = new Date(date.date);
+              // Format the date as "Sun, May 12"
+              const formattedDate = parsedDate.toLocaleString("en-US", {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+              });
+
+              return (
+                <tr
+                  key={`${date.date}-${index}`}
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    minWidth: "500px",
+                  }}
+                >
+                  <td key={`${date.date}-${index}-date`}>{formattedDate}</td>
+                  <td key={`${date.date}-${index}-minTemp`}>
+                    {date.minTemp}/{date.maxTemp}&deg;C
+                  </td>
+                  <td key={`${date.date}-${index}-description`}>
+                    {date.description}
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </>
   );
 };
